1760f45492f17bd4d94444b0b05e149c77da4ddb
Katie Blizzard <kblizzard11@gmail.com>
2019-02-07 00:40:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each { |x| new_array.push(badge_maker(x)) }
+  return new_array
+end
+
+
+
+def assign_rooms(array)
+  room_array = []
+  array.each { |x| room_array.push("Hello, #{x}! You'll be assigned to room #{(array.each_index + 1)}")
+     
+  }
+  return room_array
+  end
+
+assign_rooms(["katie", "titi"])
+
+def printer(array) 
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
+end
